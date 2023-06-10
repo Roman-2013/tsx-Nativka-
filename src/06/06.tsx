c7a27e5d8ade541b7ddbefb11c0e08bde12d379b
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-06-10 23:34:52 +0300

    lesson 6

---
+++ src/06/06.tsx
@@ -0,0 +1,31 @@
+import {ChangeEvent, MouseEvent, useState} from 'react';
+
+export const User = () => {
+    let [state, setState] = useState('')
+
+
+    const deleteUser = () => {
+        setState(' ')
+        console.log('')
+    }
+
+    const saveUser = () => {
+        console.log(state)
+    }
+
+
+    const onNameChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        setState(event.currentTarget.value)
+    }
+
+    const focusLost = () => {
+        console.log('focus lost')
+    }
+
+    return (<div>
+            <input value={state} tabIndex={1} onChange={onNameChanged}></input>
+            <button name={'deleteBaton'} onClick={deleteUser}>delete</button>
+            <button name={'saveButton'} onClick={saveUser}>save</button>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {splitIntowords} from "./01. hello test/01";
+import {User} from './06/06';
 
 
 
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <User />
   </React.StrictMode>
 );
 
